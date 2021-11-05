f3571930040099fb71e43a449aacc6c7bc080184
Bro-o <62311763+Bro-o@users.noreply.github.com>
2021-11-05 18:23:23 +0900

    React 자리 옮기기

---
+++ Bro/src/seat.js
@@ -0,0 +1,47 @@
+var button = document.getElementById("button");
+var p = document.getElementById("shop");
+var newDIV = document.createElement("div");
+button.onclick = function(){
+  newDIV.innerHTML = "1";
+  newDIV.setAttribute("id","table");
+  p.appendChild(newDIV);
+};
+
+var table = document.getElementById("table");
+
+table.onmousedown = function(event) {
+
+    let shiftX = event.clientX - table.getBoundingClientRect().left;
+    let shiftY = event.clientY - table.getBoundingClientRect().top;
+  
+    table.style.position = 'absolute';
+    table.style.zIndex = 1000;
+    document.body.append(table);
+  
+    moveAt(event.pageX, event.pageY);
+  
+    // 초기 이동을 고려한 좌표 (pageX, pageY)에서
+    // 공을 이동합니다.
+    function moveAt(pageX, pageY) {
+      table.style.left = pageX - shiftX + 'px';
+      table.style.top = pageY - shiftY + 'px';
+    }
+  
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+    }
+  
+    // mousemove로 공을 움직입니다.
+    document.addEventListener('mousemove', onMouseMove);
+  
+    // 공을 드롭하고, 불필요한 핸들러를 제거합니다.
+    table.onmouseup = function() {
+      document.removeEventListener('mousemove', onMouseMove);
+      table.onmouseup = null;
+    };
+  
+};
+  
+table.ondragstart = function() {
+    return false;
+};
